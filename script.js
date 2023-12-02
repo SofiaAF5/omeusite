8c7a4d33d32f0d76d7214435a6930ecb38c0f98e
SofiaAF5 <145346179+SofiaAF5@users.noreply.github.com>
2023-12-02 22:35:14 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,63 @@
+let person = {
+    name:"Sofia",
+    age:18,
+    student: true
+    
+     
+}
+console.log(person.name);
+console.log(person.age);
+console.log(person.student);
+
+
+let friends = [
+{
+    name: "Pedro",
+    age:18,
+    Student: true
+},
+
+ {
+    name:"Marta",
+    age:18,
+    Student: true
+ },
+
+ {
+    name:"Tomás",
+    age:20,
+    Student: true
+ }
+
+
+];
+console.log(friends[0].name);
+console.log(friends[0].age);
+console.log(friends[0].Student);
+console.log(friends[1].name);
+console.log(friends[1].age);
+console.log(friends[1].Student);
+console.log(friends[2].name);
+console.log(friends[2].age);
+console.log(friends[2].Student);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
